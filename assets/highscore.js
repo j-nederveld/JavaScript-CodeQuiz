b5dbd3f7367d25855cd891b5ceb6a8dd2e97d51f
John Nederveld <john.nederveld@gmail.com>
2020-09-16 09:56:37 -0400

    testing logic based on class activity

--- assets/highscore.js
+++ assets/highscore.js
@@ -13,30 +13,81 @@ tryAgain.addEventListener("click", (e) => {
 })
 
 
-function setParams() {
-
-    var field = 'username';
-    var url = window.location.href;
-    if(url.indexOf('?' + field + '=') === -1)
-    return false
-
-localStorage.setItem(JSON.stringify(userName), score);
-
-clearClick.addEventListener("click", (e) => {
-    localStorage.clear();
-    window.location = window.location.href.split("?")[0];
-})
-
-//get key and value from local storage
-for (i = 0; i < localStorage.length; i++) {
-   var keyName = localStorage.key(i);
-   var value = parseInt(localStorage.getItem(keyName));
-
-var li = document.createElement("li");
-let storedScores ="Username: " + JSON.parse(keyName) + " Score: " + value;
-todos = storedScores;
+function renderTodos() {
+    
+    // Render a new li for each todo
+    for (var i = 0; i < todos.length; i++) {
+      var todo = todos[i];
+  
+      var li = document.createElement("li");
+      li.textContent = todo;
+      li.setAttribute("data-index", i);
+  
+      var button = document.createElement("button");
+      button.textContent = "Complete";
+  
+      li.appendChild(button);
+      todoList.appendChild(li);
     }
+  }
 
-}
-console.log(todos);
-setParams();
+  function init() {
+    // Write code here to check if there are todos in localStorage
+    // If so, parse the value from localStorage and assign it to the todos variable
+    let storedScores = JSON.parse(localStorage.getItem("ToDo"));
+    if (storedScores !== null) {
+      todos = storedScores;
+    }
+    renderTodos();
+  }
+
+  function storeTodos() {
+    // Add code here to stringify the todos array and save it to the "todos" key in localStorage
+    localStorage.setItem("ToDo", JSON.stringify(todos));
+  }
+  
+    var todoText = todoInput.value.trim();
+  
+    // Return from function early if submitted todoText is blank
+    if (todoText === "") {
+      return;
+    }
+  
+    // Add new todoText to todos array, clear the input
+    todos.push(todoText);
+    todoInput.value = "";
+  
+    // Store updated todos in localStorage, re-render the list
+    storeTodos();
+    renderTodos();
+    init();
+  
+
+
+// function setParams() {
+
+//     var field = 'username';
+//     var url = window.location.href;
+//     if(url.indexOf('?' + field + '=') === -1)
+//     return false
+
+// localStorage.setItem(JSON.stringify(userName), score);
+
+// clearClick.addEventListener("click", (e) => {
+//     localStorage.clear();
+//     window.location = window.location.href.split("?")[0];
+// })
+
+// //get key and value from local storage
+// for (i = 0; i < localStorage.length; i++) {
+//    var keyName = localStorage.key(i);
+//    var value = parseInt(localStorage.getItem(keyName));
+
+// var li = document.createElement("li");
+// let storedScores ="Username: " + JSON.parse(keyName) + " Score: " + value;
+// todos = storedScores;
+//     }
+
+// }
+// console.log(todos);
+// setParams();
